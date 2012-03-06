483401dbabc118a7609d7f060f2704d485a1ed42
Jeffrey S. Haemer <jsh@usenix.org>
2012-03-06 11:57:09 +0000

    get the dependencies right

--- modules/bluetooth/manifests/disable.pp
+++ modules/bluetooth/manifests/disable.pp
@@ -1,18 +1,19 @@
 class bluetooth::disable inherits bluetooth {
 
+  Service['hidd'] {
+      require => undef,
+      ensure  => stopped,
+      enable  => false,
+  }
+
+  Package['bluez-utils'] {
+      ensure  => 'absent',
+      require  => Service['hidd'],
+  }
+
   Package['bluez-libs'] {
       ensure  => 'absent',
       require => Package['bluez-utils'],
   }
 
-  Package['bluez-utils'] {
-      ensure  => 'absent',
-      require => undef,
-      before  => Service['hidd'],
-  }
-
-  Service['hidd'] {
-      ensure => 'stopped',
-      enable => false,
-  }
 }
